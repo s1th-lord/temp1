d403ab7a90cce5ca395df2f0db3e8eaa2089ce4e
Ajitesh8423 <91715284+Ajitesh8423@users.noreply.github.com>
2022-10-07 19:51:24 +0530

    Update demo.cpp

--- folder-numero-uno/demo.cpp
+++ folder-numero-uno/demo.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 int main() {
-	int n,k;
-	cin>>n>>k;
+	int n,x;
+	cin>>n>>x;
 	if(n%2==0)
 	{
-	    if(k<=(n/2))
-	    cout<<(k*2-1);
+	    if(x<=(n/2))
+	    cout<<(x*2-1);
 	    else
-	    cout<<((k-(n/2))*2);
+	    cout<<((x-(n/2))*2);
 	}
 	else
 	{
-	    if(k<=(n/2)+1)
-	    cout<<(k*2-1);
+	    if(x<=(n/2)+1)
+	    cout<<(x*2-1);
 	    else
-	    cout<<((k-(n/2)-1)*2);
+	    cout<<((x-(n/2)-1)*2);
 	}
 	return 0;
 }
