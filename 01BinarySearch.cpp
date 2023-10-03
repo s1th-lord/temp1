6b774f354123d115b36794e288799c28397dd036
Sumit Kumar <65806501+Sumitk504@users.noreply.github.com>
2023-10-03 20:03:14 +0530

    Update 01BinarySearch.cpp

--- 01BinarySearch.cpp
+++ 01BinarySearch.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int Bsearch(int arr[], int n, int x){
     int low = 0, high = n-1;
-    while (low<=high)
+    while (low<high)
     {
         int mid = (low+high)/2;
         if (arr[mid]==x)
@@ -21,7 +21,7 @@ int Bsearch(int arr[], int n, int x){
 }
 int main(int argc, char const *argv[])
 {
-    int arr[] = {10,20,30,40,50,60};
+    int arr[] = {10,20,30,40,50,60,80};
     int n = sizeof(arr)/sizeof(arr[0]);
     cout<<Bsearch(arr,n,20);
     return 0;
